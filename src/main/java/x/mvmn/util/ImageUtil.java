a73e149e64a72bdb0208f7d93815c3774f6dcf02
mvmn <mvmn.inbox@gmail.com>
2016-12-30 18:48:33 +0200

    Parallelize brightness calculation

--- src/main/java/x/mvmn/util/ImageUtil.java
+++ src/main/java/x/mvmn/util/ImageUtil.java
@@ -1,22 +1,73 @@
 package x.mvmn.util;
 
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.TimeUnit;
 
 public class ImageUtil {
 
 	public static double calculateAverageBrightness(BufferedImage image) {
-		long result = 0;
+		double result = 0;
 
 		int argbs[] = image.getRGB(0, 0, image.getWidth(), image.getHeight(), new int[image.getWidth() * image.getHeight()], 0, image.getWidth());
-		for (int i = 0; i < argbs.length; i++) {
-			int argb = argbs[i];
-			result += argbToRed(argb);
-			result += argbToGreen(argb);
-			result += argbToBlue(argb);
+
+		final ForkJoinPool pool = new ForkJoinPool();
+		int cpuCores = Runtime.getRuntime().availableProcessors();
+
+		BrightnessCalcTask[] tasks = new BrightnessCalcTask[cpuCores];
+		int subArraySize = (int) Math.ceil(((double) argbs.length) / cpuCores);
+		for (int i = 0; i < cpuCores; i++) {
+			tasks[i] = new BrightnessCalcTask(Arrays.copyOfRange(argbs, subArraySize * i, Math.min(subArraySize * (i + 1), argbs.length)));
+			pool.submit(tasks[i]);
+		}
+		pool.shutdown();
+		while (!pool.isTerminated()) {
+			try {
+				pool.awaitTermination(5, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {
+			}
 		}
 
-		// 765 - 255 * 3
-		return (result / argbs.length) / 7.65d;
+		for (BrightnessCalcTask task : tasks) {
+			result += task.getRawResult();
+		}
+		result = result / tasks.length;
+		return result;
+	}
+
+	protected static class BrightnessCalcTask extends ForkJoinTask<Double> {
+		private static final long serialVersionUID = -6616059829029123394L;
+
+		protected final int argbs[];
+		protected volatile double result;
+
+		public BrightnessCalcTask(int argbs[]) {
+			this.argbs = argbs;
+		}
+
+		@Override
+		public Double getRawResult() {
+			return result;
+		}
+
+		@Override
+		protected void setRawResult(Double value) {
+		}
+
+		@Override
+		protected boolean exec() {
+			double result = 0.0d;
+			for (int i = 0; i < argbs.length; i++) {
+				int argb = argbs[i];
+				result += argbToRed(argb);
+				result += argbToGreen(argb);
+				result += argbToBlue(argb);
+			}
+			this.result = (result / argbs.length) / 7.65d;
+			return true;
+		}
 	}
 
 	public static int argbToAlpha(int argb) {
